4e466e51eb503c20ac87b505f8565ea40a4a54cc
JangJuRi <wkdwnfl119@naver.com>
2019-10-14 20:03:47 +0900

    로그인 / 로그아웃 (정보 받아오기 안함)

--- cctv/app/src/main/java/com/example/cctv/Mypageform.java
+++ cctv/app/src/main/java/com/example/cctv/Mypageform.java
@@ -1,13 +1,18 @@
 package com.example.cctv;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,12 +26,24 @@ import com.nhn.android.naverlogin.OAuthLogin;
 import com.nhn.android.naverlogin.OAuthLoginHandler;
 import com.nhn.android.naverlogin.ui.view.OAuthLoginButton;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class Mypageform extends Fragment implements View.OnClickListener{
     View v;
 
     LinearLayout LoginLayout;
     LinearLayout LockLayout;
     LinearLayout PatrolLayout;
+
+    LinearLayout myloginLayout;
+    TextView myNameText;
+    LinearLayout mylogoutLayout;
+    Button naverLogoutButton;
 
     /* 네이버 로그인 */
     private static String OAUTH_CLIENT_ID = "fJECNAV766XxJKZ4AQU8";
@@ -35,6 +52,7 @@ public class Mypageform extends Fragment implements View.OnClickListener{
 
     public static OAuthLoginButton mOAuthLoginButton;
     public static OAuthLogin mOAuthLoginInstance;
+    private Mypageform activity;
 
     public static Context mContext;
 
@@ -52,6 +70,13 @@ public class Mypageform extends Fragment implements View.OnClickListener{
 //        userInfoLayout = (LinearLayout)v.findViewById(R.id.userInfoLayout);
 //        userInfoLayout.setOnClickListener(this);
 
+        myloginLayout = (LinearLayout)v.findViewById(R.id.loginLayout);
+        myNameText = (TextView) v.findViewById(R.id.mypage_username);
+        mylogoutLayout = (LinearLayout)v.findViewById(R.id.logoutLayout);
+
+        naverLogoutButton = (Button)v.findViewById(R.id.naverLogoutButton);
+        naverLogoutButton.setOnClickListener(this);
+
         mContext = MainActivity.mContext;
 
         /* 네이버 아이디로 로그인 */
@@ -59,16 +84,18 @@ public class Mypageform extends Fragment implements View.OnClickListener{
         mOAuthLoginInstance.showDevelopersLog(true);
         mOAuthLoginInstance.init(mContext,OAUTH_CLIENT_ID,OAUTH_CLIENT_SECRET,OAUTH_CLIENT_NAME);
 
-        mOAuthLoginButton = (OAuthLoginButton) v.findViewById(R.id.naverLoginButton);
-        mOAuthLoginButton.setOAuthLoginHandler(mOAuthLoginHandler);
 
         if (mOAuthLoginInstance.getAccessToken(getActivity()) != null)
         {
             Toast.makeText(getActivity(), "로그인중", Toast.LENGTH_SHORT).show();
+            myloginLayout.setVisibility(View.GONE);
+            mylogoutLayout.setVisibility(View.VISIBLE);
         }
         else
         {
             Toast.makeText(getActivity(), "로그인중아님", Toast.LENGTH_SHORT).show();
+            myloginLayout.setVisibility(View.VISIBLE);
+            mylogoutLayout.setVisibility(View.GONE);
         }
         return v;
     }
@@ -92,11 +119,16 @@ public class Mypageform extends Fragment implements View.OnClickListener{
 //            case R.id.userInfoButton:
 //                ((MainActivity)getActivity()).UserInfo();
 //                break;
+            case R.id.naverLogoutButton:
+                mOAuthLoginInstance.logout(mContext);
+                /*새로고침*/
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.detach(this).attach(this).commit();
         }
     }
 
     /* 네이버 아이디로 로그인 */
-    static public OAuthLoginHandler mOAuthLoginHandler = new OAuthLoginHandler() {
+    public OAuthLoginHandler mOAuthLoginHandler = new OAuthLoginHandler() {
         @Override
         public void run(boolean success) {
             if (success) {
@@ -105,6 +137,8 @@ public class Mypageform extends Fragment implements View.OnClickListener{
                 long expiresAt = mOAuthLoginInstance.getExpiresAt(mContext);
                 String tokenType = mOAuthLoginInstance.getTokenType(mContext);
 
+                ProfileTask task = new ProfileTask();
+                task.execute(accessToken);
             } else {
                 String errorCode = mOAuthLoginInstance.getLastErrorCode(mContext).getCode();
                 String errorDesc = mOAuthLoginInstance.getLastErrorDesc(mContext);
@@ -112,4 +146,65 @@ public class Mypageform extends Fragment implements View.OnClickListener{
             }
         }
     };
+
+    class ProfileTask extends AsyncTask<String, Void, String> {
+        String result;
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String token = strings[0];// 네이버 로그인 접근 토큰;
+            String header = "Bearer " + token; // Bearer 다음에 공백 추가
+            try {
+                String apiURL = "https://openapi.naver.com/v1/nid/me";
+                URL url = new URL(apiURL);
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.setRequestMethod("GET");
+                con.setRequestProperty("Authorization", header);
+                int responseCode = con.getResponseCode();
+                BufferedReader br;
+                if (responseCode == 200) { // 정상 호출
+                    br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                } else {  // 에러 발생
+                    br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+                }
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = br.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                result = response.toString();
+                br.close();
+                System.out.println(response.toString());
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            //result 값은 JSONObject 형태로 넘어옵니다.
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                //넘어온 result 값을 JSONObject 로 변환해주고, 값을 가져오면 되는데요.
+                // result 를 Log에 찍어보면 어떻게 가져와야할 지 감이 오실거에요.
+                JSONObject object = new JSONObject(result);
+                if (object.getString("resultcode").equals("00")) {
+                    JSONObject jsonObject = new JSONObject(object.getString("response"));
+                    Log.d("jsonObject", jsonObject.toString());
+                    Log.i("테스트", result);
+//                    editor.putString("email", jsonObject.getString("email"));
+//                    editor.putString("name", jsonObject.getString("name"));
+//                    editor.putString("nickname", jsonObject.getString("nickname"));
+//                    editor.putString("profile", jsonObject.getString("profile_image"));
+//                    editor.apply();
+
+                    ((MainActivity) getActivity()).Mainpage();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
