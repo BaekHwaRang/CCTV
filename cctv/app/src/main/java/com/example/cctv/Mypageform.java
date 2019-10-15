c2d4f5990adcfd39356005018687ca1efe685882
JangJuRi <wkdwnfl119@gmai.ocm>
2019-10-15 10:47:45 +0900

    프로필 이미지 연동

--- cctv/app/src/main/java/com/example/cctv/Mypageform.java
+++ cctv/app/src/main/java/com/example/cctv/Mypageform.java
@@ -4,10 +4,13 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -18,6 +21,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -30,6 +34,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -45,6 +50,7 @@ public class Mypageform extends Fragment implements View.OnClickListener{
     TextView myNameText;
     LinearLayout mylogoutLayout;
     Button naverLogoutButton;
+    ImageView profileImage;
 
     /* 네이버 로그인 */
     private static String OAUTH_CLIENT_ID = "fJECNAV766XxJKZ4AQU8";
@@ -53,7 +59,8 @@ public class Mypageform extends Fragment implements View.OnClickListener{
 
     public static OAuthLoginButton mOAuthLoginButton;
     public static OAuthLogin mOAuthLoginInstance;
-    private Mypageform activity;
+
+    Handler handler = new Handler();
 
     public static Context mContext;
 
@@ -62,8 +69,6 @@ public class Mypageform extends Fragment implements View.OnClickListener{
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.mypageform, container, false);
 
-        LoginLayout = (LinearLayout)v.findViewById(R.id.mypage_login_layout);
-        LoginLayout.setOnClickListener(this);
         LockLayout = (LinearLayout)v.findViewById(R.id.mypage_lockLayout);
         LockLayout.setOnClickListener(this);
         PatrolLayout = (LinearLayout)v.findViewById(R.id.mypage_patrolLayout);
@@ -71,9 +76,11 @@ public class Mypageform extends Fragment implements View.OnClickListener{
 //        userInfoLayout = (LinearLayout)v.findViewById(R.id.userInfoLayout);
 //        userInfoLayout.setOnClickListener(this);
 
-        myloginLayout = (LinearLayout)v.findViewById(R.id.loginLayout);
+        myloginLayout = (LinearLayout)v.findViewById(R.id.mypage_login_layout);
+        myloginLayout.setOnClickListener(this);
         myNameText = (TextView) v.findViewById(R.id.mypage_username);
-        mylogoutLayout = (LinearLayout)v.findViewById(R.id.logoutLayout);
+        mylogoutLayout = (LinearLayout)v.findViewById(R.id.mypage_logout_layout);
+        profileImage = (ImageView)v.findViewById(R.id.userProfileImage);
 
         naverLogoutButton = (Button)v.findViewById(R.id.naverLogoutButton);
         naverLogoutButton.setOnClickListener(this);
@@ -90,15 +97,15 @@ public class Mypageform extends Fragment implements View.OnClickListener{
             new RequestApiTask().execute();
             myNameText.setText("");
             Toast.makeText(getActivity(), "로그인중", Toast.LENGTH_SHORT).show();
-            myloginLayout.setVisibility(View.GONE);
-            mylogoutLayout.setVisibility(View.VISIBLE);
+            mylogoutLayout.setVisibility(View.GONE);
+            myloginLayout.setVisibility(View.VISIBLE);
 
         }
         else
         {
             Toast.makeText(getActivity(), "로그인중아님", Toast.LENGTH_SHORT).show();
-            myloginLayout.setVisibility(View.VISIBLE);
-            mylogoutLayout.setVisibility(View.GONE);
+            mylogoutLayout.setVisibility(View.VISIBLE);
+            myloginLayout.setVisibility(View.GONE);
         }
         return v;
     }
@@ -107,9 +114,9 @@ public class Mypageform extends Fragment implements View.OnClickListener{
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.mypage_login_layout:
-                ((MainActivity)getActivity()).Login_select();
-                break;
+//            case R.id.mypage_login_layout:
+//                ((MainActivity)getActivity()).UserInfo();
+//                break;
             case R.id.mypage_lockLayout:
                 Intent intent = new Intent(getActivity(),Mypage_Lockform.class);
                 startActivity(intent);
@@ -166,11 +173,36 @@ public class Mypageform extends Fragment implements View.OnClickListener{
                 JSONObject jsonObject = new JSONObject(content);
                 JSONObject response = jsonObject.getJSONObject("response");
                 String name = response.getString("name");
+                final String image = response.getString("profile_image");
                 myNameText.setText(name);//메일 란 채우기
+
+                Thread t = new Thread(new Runnable() {
+                    @Override
+                    public void run() {    // 오래 거릴 작업을 구현한다
+                        try{
+                            // 걍 외우는게 좋다 -_-;
+                            final ImageView iv = (ImageView)v.findViewById(R.id.userProfileImage);
+                            URL url = new URL(image);
+                            InputStream is = url.openStream();
+                            final Bitmap bm = BitmapFactory.decodeStream(is);
+                            handler.post(new Runnable() {
+
+                                @Override
+                                public void run() {  // 화면에 그려줄 작업
+                                    iv.setImageBitmap(bm);
+                                }
+                            });
+                            iv.setImageBitmap(bm); //비트맵 객체로 보여주기
+                        } catch(Exception e){
+                        }
+                    }
+                });
+                t.start();
             }
             catch (Exception e){
                 e.printStackTrace();
             }
         }
+
     }
 }
